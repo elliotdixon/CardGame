f2db49bb8c4ebc7cf06b826e4978216edbd09957
Elliot Dixon <elliotjohndixon@gmail.com>
2019-11-21 13:25:49 +0000

    deck setup

---
+++ src/main/java/Deck.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Deck {
+    private ArrayList<Card> cardsInDeck;
+
+    public Deck() {
+        this.cardsInDeck = new ArrayList<Card>();
+
+        for (SuitType suit:SuitType.values()) {
+            for (RankType rank:RankType.values()) {
+                cardsInDeck.add(new Card(suit, rank));
+            }
+        }
+    }
+
+    public ArrayList<Card> getCardsInDeck() {
+        return cardsInDeck;
+    }
+
+    public Card removeCard() {
+        return cardsInDeck.remove(0);
+    }
+
+    public Card getCard() {
+        return cardsInDeck.get(0);
+    }
+
+    public void shuffleDeck(){
+        Collections.shuffle(cardsInDeck);
+    }
+
+
+}
---
+++ src/test/java/DeckTest.java
@@ -0,0 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DeckTest {
+
+    private Deck myDeck;
+
+    @Before
+    public void before() {
+        myDeck = new Deck();
+    }
+
+
+    @Test
+    public void myDeckHas52Cards() {
+        assertEquals(52, myDeck.getCardsInDeck().size());
+    }
+
+    @Test
+    public void firstCardInDeckIsAceOfHearts() {
+        assertEquals(1, myDeck.getCard().getRank().getValue());
+        assertEquals("HEARTS", myDeck.getCard().getSuit().name());
+        assertEquals(RankType.ACE, myDeck.getCard().getRank());
+    }
+
+    @Test
+    public void removeCardFirstCardInDeckIsNowTwoOfHearts() {
+        myDeck.removeCard();
+        assertEquals(2, myDeck.getCard().getRank().getValue());
+        assertEquals("HEARTS", myDeck.getCard().getSuit().name());
+        assertEquals(RankType.TWO, myDeck.getCard().getRank());
+    }
+
+}
